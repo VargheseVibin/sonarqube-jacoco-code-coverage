fd1f7dec46effd4d5c4b499c321a28564645179f
VargheseVibin <34751841+VargheseVibin@users.noreply.github.com>
2020-12-11 02:00:16 +0530

    Create MathService01.java

---
+++ src/main/java/com/tom/service/MathService01.java
@@ -0,0 +1,15 @@
+
+package com.tom.service;
+
+public class MathService01 {
+    public int multiply(int firstNumber, int secondNumber) {
+
+        return firstNumber * secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        Integer myInt = null;
+        Integer yourInt = myInt * 2;
+        return firstNumber - secondNumber;
+    }
+}
